dd2eca95ba67b40868b45cd659cb09eb0cf1fa45
Ezzdin Atef <ezzdin1125@gmail.com>
2020-08-18 19:19:02 +0200

    make it better

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port 5000");
 });

bad0acaf9b5e9a737230d83a9c8b02c4ea3a1197
Ezzdin Atef <ezzdin1125@gmail.com>
2020-08-25 03:41:58 +0200

    add the checkout session and update the buy query

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-app.post("/create-checkout-session", async (req, res) => {
+app.post("/create-checkout-session", cors(), async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [

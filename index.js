1c3140a4bdc2890c2b7ab87f320a8a2e7457b4de
Ezzdin Atef <ezzdin1125@gmail.com>
2020-08-25 23:19:59 +0200

    update the cors

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ mongoose.connection.once("open", () => {
 });
 
 const corsOptions = {
-  origin: 'https://ecommerce-react-123.netlify.app',
+  origin: 'https://ecommerce-react-123.netlify.app/',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(corscorsOptions));
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 
